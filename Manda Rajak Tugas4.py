44728bbfe55d12c678cf6471be83743ce535cc6b
mandarajak <162272508+mandarajak@users.noreply.github.com>
2024-03-20 23:17:06 +0900

    Add files via upload

---
+++ Manda Rajak Tugas4.py
@@ -0,0 +1,9 @@
+bilangan = int (input("masukan bilangan bulat:"))
+
+hexa = hex(bilangan)
+binary = bin(bilangan)
+octal = oct(bilangan)
+
+print ("bilangan heksadesimal :", hexa)
+print ("bilangan biner:", binary)
+print ("bilangan oktal:", octal)
